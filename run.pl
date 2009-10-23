96faa81ee0da6975fedbfd8d62854a8d746e38f0
Jan Wielemaker <J.Wielemaker@uva.nl>
2009-10-23 13:25:08 +0200

    Update documentation

--- run.pl
+++ run.pl
@@ -4,13 +4,29 @@
 
 :- debug(oai).
 
+%%	sp(+ServerID) is det.
+%
+%	Fetch server properties and store them into an RDF graph named
+%	ServerID.
+
 sp(Server) :-
 	oai_server_properties(Server, Server).
+
+%%	gr(+ServerID) is det.
+%
+%	Fetch all records for ServerID and store them in the named graph
+%	ServerID. This works for  relatively   small  repositories.  For
+%	*big* repositories, see oai_crawl/3.
 
 gr(Server) :-
 	oai_records(Server, Server,
 		    [
 		    ]).
+
+%%	gr1(+ServerID) is det.
+%
+%	As gr/1, but does not resume.
+
 gr1(Server) :-
 	oai_records(Server, Server,
 		    [ resume(false)
